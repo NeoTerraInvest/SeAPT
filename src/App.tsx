4348adfdd4db534df64aea15d28130329811e259
4BFC <4bee.code@gmail.com>
2025-02-24 16:38:45 +0900

    config: create main components and implement them in the `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-import Test from './components/Test/Test';
+import { Test } from './components';
+import { Main as Home } from './pages';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<div>Home</div>} />
+      <Route path='/' element={<Home />} />
       <Route path='/test' element={<Test />} />
     </Routes>
   );
---
+++ src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/main.module.scss';
+const Main = () => {
+  return (
+    <div id={styles.debug}>
+      Home
+      <span>Ready for Launch🚀</span>
+    </div>
+  );
+};
+
+export default Main;
