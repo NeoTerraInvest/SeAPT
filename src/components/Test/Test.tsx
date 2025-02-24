ab4bcc071cf1aeb619c013c39fdf7fd773f9a2e5
4BFC <4bee.code@gmail.com>
2025-02-24 14:35:26 +0900

    config: applied scss formatted files

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -1,34 +1,40 @@
 import { useState } from "react";
 import { react } from "../../assets";
 import viteLogo from "/vite.svg";
-import "../../App.scss";
+import styles from "../../styles/test.module.scss";
 
 function Test() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
+    <div id={styles.debug}>
+      <div className={styles.contents}>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className={styles.logo} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={react} className="logo react" alt="React logo" />
+          <img
+            src={react}
+            className={`${styles.logo} ${styles.react}`}
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className={styles.card}>
+        <div id={styles.button}>
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className={`${styles.readTheDocs}`}>
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
