5808d17f18aba720d0bb8e07c93ae491856ad418
4BFC <4bee.code@gmail.com>
2025-03-04 17:44:20 +0900

    feat: remaned Slider to SliderFlex
    - SliderFlex edited to be possible and dynamically changed based on the prop type

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,7 +5,7 @@ export { default as BaseLayout } from './BaseLayout/BaseLayout';
 export { default as MarginLayout } from './BaseLayout/MarginLayout';
 export { default as Show } from './Show/Show';
 export { default as Tokenomics } from './Tokenomics/Tokenomics';
+export { default as About } from './About/About';
 //ui
 export { default as AboutFrame } from './ui/AboutFrame';
-export { default as TokenFrameTest } from './ui/TokenFram.test';
 export { default as TokenFrame } from './ui/TokenFrame';
