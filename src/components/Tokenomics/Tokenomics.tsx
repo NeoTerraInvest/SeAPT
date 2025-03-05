b637b3a736e74ddf14581d92e6a8a7cafc1d1347
4BFC <4bee.code@gmail.com>
2025-03-05 15:47:55 +0900

    feat: implement screen aspect ratio tracking using useEffect

--- src/components/Tokenomics/Tokenomics.tsx
+++ src/components/Tokenomics/Tokenomics.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { TokenFrame } from '@components';
 import { tokenomics as rawStyles } from '@/styles';
 import { SliderFlex } from '@model';
@@ -6,8 +7,22 @@ import { DefaultStyled } from '@/types';
 const styles = rawStyles as unknown as DefaultStyled;
 
 const Tokenomics = () => {
+  const [isMobile, setMobile] = useState<boolean>(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const mobileState = window.innerWidth <= 768;
+      setMobile(mobileState);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <SliderFlex num={4} styles={styles} type={false}>
+    <SliderFlex num={4} styles={styles} type={isMobile}>
       <TokenFrame />
     </SliderFlex>
   );
