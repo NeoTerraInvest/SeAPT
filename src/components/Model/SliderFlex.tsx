fd06e3f6d4209fabaec720b71af3f3d8c5a9dc64
4BFC <4bee.code@gmail.com>
2025-03-05 14:24:45 +0900

    feat: SliderFlex test

--- src/components/Model/SliderFlex.tsx
+++ src/components/Model/SliderFlex.tsx
@@ -9,15 +9,16 @@ const debugStyled: DefaultStyled = {
   debug: Error.debug,
   container: Error.container,
   element: Error.element,
+  containerRow: Error.containerRow,
 };
 
 /**
  * ---
- * CSS types for the **styles** props should use the **className**
+ * The **styles** props, CSS types should be applied using the **className**, This components is designed solely for HTML structure and flex layout, not for mediaQuery.
  * @param {object} props
  * @param {ReactNode} props.childern - *necessary
  * @param {number} props.num - *necessary
- * @param {boolean} props.type - optional / default: true
+ * @param {string} props.type - optional / default: 'singleCol' / case: 'singleCol' | 'dobuleCol' | 'singleRow'
  * @param {DefaultStyled} [props.styles=debugStyled] - debugStyled / DefaultStyled from **global.d.ts**
  * @example
  * ```
@@ -25,49 +26,69 @@ const debugStyled: DefaultStyled = {
  *    debug: Error.debug,
  *    container: Error.container,
  *    element: Error.element,
+ *    containerRow: Error.containerRow,
  * };
  * ```
  * @returns {JSX.Element} JSX.Element - `return JSX.Element` can dynamically change `style module` based on the **type** state
  */
 const SliderFlex = ({
   children,
-  type = true,
+  type = 'singleCol',
   num,
   styles = debugStyled,
 }: {
   children: ReactNode;
-  type?: boolean;
+  type?: 'singleCol' | 'dobuleCol' | 'singleRow';
   num: number;
   styles?: DefaultStyled;
 }) => {
-  return (
-    <section className={styles.debug}>
-      {type
-        ? //sigle col
-          new Array(num).fill(null).map((_, i) =>
-            i !== null ? (
-              <div key={i} className={styles.container}>
-                <div className={styles.element}>{children}</div>
-              </div>
-            ) : (
-              <span className={styles.debug}>something is wrong</span>
-            ),
-          )
-        : //dobule col
-          new Array(num).fill(null).map((_, i) =>
-            i !== null ? (
-              i % 2 === 0 ? (
-                <div key={i} className={styles.container}>
-                  <div className={styles.element}>{children}</div>
-                  <div className={styles.element}>{children}</div>
-                </div>
-              ) : null
-            ) : (
-              <span className={styles.debug}>something is wrong</span>
-            ),
-          )}
-    </section>
-  );
+  let flex;
+  switch (type) {
+    case 'singleCol':
+      flex = new Array(num).fill(null).map((_, i) =>
+        i !== null ? (
+          <div key={i} className={styles.container}>
+            <div className={styles.element}>{children}</div>
+          </div>
+        ) : (
+          <span className={styles.debug}>something is wrong</span>
+        ),
+      );
+      break;
+    case 'dobuleCol':
+      flex = new Array(num).fill(null).map((_, i) =>
+        i !== null ? (
+          i % 2 === 0 ? (
+            <div key={i} className={styles.container}>
+              <div className={styles.element}>{children}</div>
+              <div className={styles.element}>{children}</div>
+            </div>
+          ) : null
+        ) : (
+          <span className={styles.debug}>something is wrong</span>
+        ),
+      );
+      break;
+    case 'singleRow':
+      flex = new Array(num).fill(null).map((_, i) =>
+        i !== null ? (
+          <div key={i} className={styles.containerRow}>
+            <div className={styles.element}>{children}</div>
+          </div>
+        ) : (
+          <span className={styles.debug}>something is wrong</span>
+        ),
+      );
+      break;
+    default:
+      flex = (
+        <span className={styles.debug}>
+          ⚠️ Unsupported type: <strong>{type}</strong>
+        </span>
+      );
+  }
+
+  return <section className={styles.debug}>{flex}</section>;
 };
 
 export default SliderFlex;
