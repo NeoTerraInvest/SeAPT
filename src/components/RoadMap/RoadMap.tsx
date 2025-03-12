d481842cf7f3a735341c02bbfb362bc0bbfb7666
4BFC <4bee.code@gmail.com>
2025-03-12 14:17:45 +0900

    feat: implement sencer in the effect div to detect scrolling

--- src/components/RoadMap/RoadMap.tsx
+++ src/components/RoadMap/RoadMap.tsx
@@ -1,32 +1,32 @@
-import { ReactNode } from 'react';
+import { ReactNode, useRef } from 'react';
 import { roadMap as styles } from '@styles';
 
 const RoadMap = ({ children, num }: { children: ReactNode; num: number }) => {
-  // const containerRef = useRef<HTMLDivElement>(null);
+  const effectRef = useRef<HTMLDivElement>(null);
 
-  // const handleInMouse = () => {
-  //   console.log('Mouse in');
-  //   autoClick();
-  // };
-  // const hanldeOutMouse = () => {
-  //   console.log('Mouse out');
-  // };
-  // const autoClick = () => {
-  //   console.log('Auto Click');
-  //   if (containerRef.current) {
-  //     containerRef.current.click();
-  //   }
-  // };
+  const handleInMouse = () => {
+    // console.log('Mouse in');
+    autoClick();
+  };
+  const hanldeOutMouse = () => {
+    // console.log('Mouse out');
+  };
+  const autoClick = () => {
+    // console.log('Auto Click');
+    if (effectRef.current) {
+      effectRef.current.click();
+    }
+  };
 
   return (
-    <div
-      // ref={containerRef}
-      className={styles.debug}
-      // onMouseEnter={handleInMouse}
-      // onMouseLeave={hanldeOutMouse}
-    >
-      <div className={styles.effectLayout}>
-        <div id={styles.effectTop} />
+    <div ref={effectRef} className={styles.debug}>
+      <div
+        ref={effectRef}
+        onMouseEnter={handleInMouse}
+        onMouseLeave={hanldeOutMouse}
+        className={styles.effectLayout}
+      >
+        <div ref={effectRef} id={styles.effectTop} />
         <div className={styles.container}>
           {new Array(num).fill(null).map((_, i) => {
             return (
@@ -36,7 +36,12 @@ const RoadMap = ({ children, num }: { children: ReactNode; num: number }) => {
             );
           })}
         </div>
-        <div id={styles.effectBottom} />
+        <div
+          ref={effectRef}
+          onMouseEnter={handleInMouse}
+          onMouseLeave={hanldeOutMouse}
+          id={styles.effectBottom}
+        />
       </div>
     </div>
   );
