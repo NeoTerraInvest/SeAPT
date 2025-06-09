3845011ed840c1da8c6c8945227db716a853645d
4Bee <109135643+4BFC@users.noreply.github.com>
2025-06-09 14:14:50 +0900

    Config: 디자인 시스템 설정 (#74)
    * feat: 전체 페이지를 점검하고 리팩터링할 디렉토리 설정
    
    * config: 개발 환경을 개선하기 위해 issue, pr 템플릿 생성
    
    * config: tailwindCSS로 기본 디자인 시스템 적용
    
    - tailwindCSS v4 특징에 맞게 globals.css에 디자인 시스템 코드를 구현
    
    * config: prettier 설치 및 적용
    
    - prettier-plugin-tailwindcss 플러그인 적용
    
    * confit: tailwindCSS config파일 설정
    
    - tailwindCSS의 유틸리티 시스템을 config 파일에서 관리하도록 설정
    
    ---------
    
    Co-authored-by: 4BFC <4bee.code@gamil.com>

--- next_client/src/app/page.tsx
+++ next_client/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="container mx-auto">
+      <h1 className="bg-primary">Hello World</h1>
     </div>
   );
 }
